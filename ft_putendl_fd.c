d473e6543c8b56581db2f073081c49c732175e57
uygarpolat <uygarpolat@gmail.com>
2024-04-12 23:25:04 +0300

    ft_putstr_fd.c ft_putendl_fd.c

---
+++ ft_putendl_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 23:19:56 by upolat            #+#    #+#             */
+/*   Updated: 2024/04/12 23:23:59 by upolat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_putendl_fd(argv[1], 1);
+	return (0);
+}
+*/

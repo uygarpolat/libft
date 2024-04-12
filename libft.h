0b06142a401105c061fa5f90cf97914700597cff
uygarpolat <uygarpolat@gmail.com>
2024-04-12 03:09:28 +0300

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 22:44:36 by upolat            #+#    #+#             */
-/*   Updated: 2024/04/12 01:00:26 by upolat           ###   ########.fr       */
+/*   Updated: 2024/04/12 03:06:25 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,14 @@ char			*ft_strdup(char *str);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
-char 			**ft_split(char const *s, char c);
-char 			*ft_itoa(int n);
-char 			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void 			ft_striteri(char *s, void (*f)(unsigned int, char*));
-void 			ft_putchar_fd(char c, int fd);
-void 			ft_putstr_fd(char *s, int fd);
-void 			ft_putendl_fd(char *s, int fd);
-void 			ft_putnbr_fd(int n, int fd);
+char			**ft_split(char const *s, char c);
+char			*ft_itoa(int n);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+char			*ft_itoa(int n);
 
 #endif

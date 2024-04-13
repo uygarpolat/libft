030a1330719fa326626420ccb750d584efd243f9
uygarpolat <uygarpolat@gmail.com>
2024-04-14 02:19:07 +0300

    ft_strtrim.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 03:05:59 by upolat            #+#    #+#             */
-/*   Updated: 2024/04/13 03:11:20 by upolat           ###   ########.fr       */
+/*   Updated: 2024/04/14 02:10:01 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*chars1;
-	char	*chars2;
+	const char	*chars1;
+	const char	*chars2;
 
 	chars1 = (const char *)s1;
 	chars2 = (const char *)s2;

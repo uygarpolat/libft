804297f5548f35803943d10da6e93277bd6acee0
uygarpolat <uygarpolat@gmail.com>
2024-04-16 20:45:55 +0300

    ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 23:04:19 by upolat            #+#    #+#             */
-/*   Updated: 2024/04/08 01:33:14 by upolat           ###   ########.fr       */
+/*   Updated: 2024/04/16 20:44:25 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,10 @@ int	ft_atoi(const char *str)
 			break ;
 	}
 	if (str[i] == '-')
+	{
 		sign = -sign;
-	i++;
+		i++;
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		nbr = nbr * 10 + str[i] - '0';
@@ -36,13 +38,16 @@ int	ft_atoi(const char *str)
 	}
 	return (sign * nbr);
 }
-/*
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int	main(void)
+int main(int argc, char **argv)
 {
-	printf("%d|%d\n", atoi("\r\n\t\b\f\v\a-42"), ft_atoi("\r\n\t\b\f\v\a-42"));
-	return (0);
+	if (argc == 2)
+	{
+		printf("                                atoi returns:    %d\n                                ft_atoi returns: %d\n                                strtol returns:  %d\n", atoi(argv[1]), atoi(argv[1]), ft_atoi(argv[1]));
+		return (0);
+	}
 }
-*/
+
